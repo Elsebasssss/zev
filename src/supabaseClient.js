42b0037e9db63fa6ac0d62229da7276f7c5a2e70
Elsebassss <sebastiansalinas627@gmail.com>
2026-08-31 12:15:46 -0500

    Fix: credenciales anon de Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jtykpayixzxahabxfvot.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0eWtwYXlpeHp4YWhhYnhmdm90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxODc2MzgsImV4cCI6MjEwMzc2MzYzOH0.Z47dY9hdZ8alSTZyEMjTfyDXbWjkomsHxw64QpV7dSI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
